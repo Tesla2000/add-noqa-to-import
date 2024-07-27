1284335f4250ac118ed536b4ca1e498cab1a9a27
Tesla2000 <fratajczak124@gmail.com>
2024-07-27 20:12:36 +0200

    Updated test and poetry file

--- test.py
+++ test.py
@@ -7,10 +7,6 @@ from add_noqa_to_import import fix_imports
 
 class TestAddComments(TestCase):
     def test_add_comments(self):
-        shutil.copyfile(
-            'test_folders/backup_test_files/uncommented_file.py',
-            'test_folders/test_folder1/uncommented_file.py',
-        )
         self.assertEqual(
             Path('test_folders/backup_test_files/commented_file.py').read_text(),
-            fix_imports(Path('test_folders/test_folder1/uncommented_file.py').read_text()))
+            fix_imports(Path('test_folders/backup_test_files/uncommented_file.py').read_text()))
